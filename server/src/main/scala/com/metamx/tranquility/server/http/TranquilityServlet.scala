d89f90110416b6a526c7be472e600e4c5b769b8a
Anna Scholtz <anna@scholtzan.net>
2019-08-15 14:52:22 -0700

    Add flattenSpec

--- server/src/main/scala/com/metamx/tranquility/server/http/TranquilityServlet.scala
+++ server/src/main/scala/com/metamx/tranquility/server/http/TranquilityServlet.scala
@@ -117,7 +117,9 @@ class TranquilityServlet(
       case Some(JsonContentType) | Some(SmileContentType) =>
         Messages.fromObjectStream(decompressor(request.inputStream), forceDataSource, objectMapper) map {
           case (dataSource, d) =>
-            val row = getBundle(dataSource).mapParser.parse(d.asJava.asInstanceOf[java.util.Map[String, AnyRef]])
+            val mapper = new ObjectMapper()
+            val res = mapper.writeValueAsString(d.asJava.asInstanceOf[java.util.Map[String, AnyRef]])
+            val row = getBundle(dataSource).stringParser.parse(res)
             (dataSource, row)
         }
 
